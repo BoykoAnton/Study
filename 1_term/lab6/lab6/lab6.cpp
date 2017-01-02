bfa7e6693e7c9179d2d5371b429e9b2a78f02860
BoykoAnton <boyko.anton.mp@gmail.com>
2017-01-02 11:59:15 +0200

    1 modul labs 1-11

---
+++ 1_term/lab6/lab6/lab6.cpp
@@ -0,0 +1,28 @@
+// lab6.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+int main()
+{
+	char c1 = 48,
+		 c2 = 57,
+		 c3 = 98;
+
+	const char C1 = '%',
+			   C2 = 'H',
+			   C3 = 's';
+
+	const char A = '9',
+	           B = 0x3;
+
+	char c = 'Q',
+		 d = 0x67,
+		 q,
+		 w;
+
+	q = '#';
+	w = 0x36;
+    
+	return 0;
+}
